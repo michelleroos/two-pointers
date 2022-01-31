42d764966bfb721e03a44b7dea55e9702223d5b4
michelleroos <hello.michelleroos@gmail.com>
2022-01-30 21:35:04 -0500

    pair target sum

--- pair-target-sum.js
+++ pair-target-sum.js
@@ -5,11 +5,51 @@ Given an array of sorted numbers and a target sum, find a pair in the array whos
 Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
 */
 
+// USING TWO POINTERS
+// const pair_with_targetsum = function (arr, target_sum) {
+//   let end = arr.length - 1;
+//   let start = 0;
+//   while (start < end) {
+//     const sum = arr[start] + arr[end];
+//     if (sum === target_sum) {
+//       return [start, end];
+//     }
+//     if (sum < target_sum) {
+//       start++;
+//     } else {
+//       end--;
+//     }
+//   }
+// }
 
-const arr1 =  [1, 2, 3, 4, 6], target1 = 6
+// USING HASHMAP
+//                0  1  2  3  4
+// const arr1 =  [1, 2, 3, 4, 6], target1 = 6;
+const pair_with_targetsum = function (arr, target_sum) {
+  const nums = {}; // [1:0, ]
+  for (let i = 0; i < arr.length; i++) { // 1
+    const num = arr[i]; // 2
+    if ((target_sum - num) in nums) {
+      return [nums[target_sum - num], i];
+    }
+    nums[num] = i;
+  }
+}
+
+/*
+Time Complexity#
+The time complexity of the above algorithm will be O(N)O(N), where ‘N’ is the total number of elements in the given array.
+
+Space Complexity#
+The algorithm runs in constant space O(1)O(1).
+*/
+
+const arr1 =  [1, 2, 3, 4, 6], target1 = 6;
+console.log(pair_with_targetsum(arr1, target1));
 // Output: [1, 3]
 // Explanation: The numbers at index 1 and 3 add up to 6: 2+4=6
 
-const arr2 =  [2, 5, 9, 11], target2 = 11
+const arr2 =  [2, 5, 9, 11], target2 = 11;
+console.log(pair_with_targetsum(arr2, target2));
 // Output: [0, 2]
 // Explanation: The numbers at index 0 and 2 add up to 11: 2+9=11
